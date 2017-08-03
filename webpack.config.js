6ea85683612221ea3a493fb582e28e6dd594686b
Phoebe Greig <Phoebe.Greig@gmail.com>
2017-08-03 09:43:23 +0100

    basic prod setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,29 +3,30 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './app/index.js',
-  output: {
-    path: __dirname,
-    filename: 'bundle.js',
-  },
-  module: {
-    loaders: [
-      {
-        test: /.js$/,
-        loader: 'babel-loader',
-        include: path.join(__dirname, 'app'),
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-        template: './app/index.html',
-        filename: 'index.html',
-        inject: 'body',
-    })
+    devtool: 'inline-source-map',
+    entry: ['./app/index.js'],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                include: path.join(__dirname, 'app'),
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './app/public/index.html',
+            filename: 'index.html',
+            inject: 'body',
+        }),
     ],
 };
